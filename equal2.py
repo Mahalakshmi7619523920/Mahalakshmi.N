d4401407832a04c12c660ce6b73ef97f954b63e7
Mahalakshmi7619523920 <48116520+Mahalakshmi7619523920@users.noreply.github.com>
2019-03-25 10:04:52 +0530

    Add files via upload

---
+++ equal2.py
@@ -0,0 +1,7 @@
+n1 = int(input("enter first number:"))
+n2 = int(input("enter second number:"))
+n3 = int(input("enter third number:"))
+sum=n1+n2+n3
+print("sum",sum)
+if n1==n2 or n2==n3 or n3==n1:
+	print("0")
